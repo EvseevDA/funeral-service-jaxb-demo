b8935300e20838d2d35457bfd27e6bc2eec578e8
EvseevDA <127671691+ne.evseev.da@users.noreply.github.com>
2024-04-28 11:46:06 +0300

    Second checkpoint

--- src/main/java/com/funeralservice/Main.java
+++ src/main/java/com/funeralservice/Main.java
@@ -1,6 +1,7 @@
 package com.funeralservice;
 
 import com.funeralservice.xml.service.jaxb.OrderServiceJaxb;
+import com.funeralservice.xml.service.xpath.ClientServiceXPath;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -10,12 +11,34 @@ public class Main {
 
     public static void main(String[] args) {
         OrderServiceJaxb orderServiceJaxb = new OrderServiceJaxb();
-        System.out.println(orderServiceJaxb.getWhereTotalPriceGreaterThan(BigDecimal.valueOf(7000)));
-        printList(orderServiceJaxb.getAll(), '\n');
+        System.out.println("Request: orderServiceJaxb.getWhereTotalPriceGreaterThan(BigDecimal.valueOf(7000))\n");
+        System.out.println(orderServiceJaxb.getWhereTotalPriceGreaterThan(BigDecimal.valueOf(7000)) + "\n\n");
+
+        System.out.println("Request: orderServiceJaxb.getAll()\n");
+        printList(orderServiceJaxb.getAll(), "\n");
+        System.out.println();
+
+        System.out.println("Request: orderServiceJaxb.getTotalPricePerAllOrders()\n");
         System.out.println(orderServiceJaxb.getTotalPricePerAllOrders());
+        System.out.println("\n\n");
+
+        ClientServiceXPath clientServiceXPath = new ClientServiceXPath();
+        System.out.println("Got all clients with XPath\n\n");
+        clientServiceXPath
+                .getAll()
+                .forEach(c -> {
+                    printList(c, "\n");
+                    System.out.println('\n');
+                });
+        System.out.println("\n");
+
+        System.out.println("Got client by id with XPath\n");
+        clientServiceXPath
+                .getById(0)
+                .ifPresent(c -> printList(c, "\n"));
     }
 
-    static void printList(List<?> list, char separator) {
+    static void printList(List<?> list, String separator) {
         list.forEach(e -> System.out.print(Objects.toString(e) + separator));
     }
 }
